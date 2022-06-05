5f71889e4abcb499a75eb50e7c60b0ebcf033b59
dotatthanh <thanhbuon1910@gmail.com>
2022-06-05 10:54:36 +0700

    fix menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -58,7 +58,7 @@
                 </li>
                 @endcan
 
-                @can('Xem danh sách giấy hẹn tái khám')
+                @can('Xem danh sách giấy hẹn khám bệnh')
                 <li>
                     <a href="{{ route('appointment_papers.index') }}" class=" waves-effect">
                         <i class="bx bx-calendar"></i>
